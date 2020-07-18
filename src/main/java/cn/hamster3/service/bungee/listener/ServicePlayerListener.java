d9f545b76244a4c4ec693b01d14e46168c6bcc56
MiniDay <372403923@qq.com>
2020-07-18 17:31:42 +0800

    修复了了判断玩家是否在bc服务器上的方法出现的bug

--- src/main/java/cn/hamster3/service/bungee/listener/ServicePlayerListener.java
+++ src/main/java/cn/hamster3/service/bungee/listener/ServicePlayerListener.java
@@ -1,18 +1,29 @@
 package cn.hamster3.service.bungee.listener;
 
+import cn.hamster3.service.bungee.event.ServiceClientConnectedEvent;
 import cn.hamster3.service.bungee.service.ServiceGroup;
 import cn.hamster3.service.bungee.service.ServiceManager;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.PlayerDisconnectEvent;
 import net.md_5.bungee.api.event.ServerConnectedEvent;
-import net.md_5.bungee.api.event.ServerDisconnectEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 public class ServicePlayerListener implements Listener {
+    private final HashMap<UUID, String> onlinePlayer;
+
+    public ServicePlayerListener() {
+        onlinePlayer = new HashMap<>();
+    }
+
     @EventHandler
     public void onPlayerConnected(ServerConnectedEvent event) {
         ProxiedPlayer player = event.getPlayer();
+        onlinePlayer.put(player.getUniqueId(), player.getName());
         for (ServiceGroup group : ServiceManager.getGroups()) {
             group.broadcast("HamsterService", "playerConnected %s %s", player.getUniqueId(), player.getName().toLowerCase());
         }
@@ -21,10 +32,16 @@ public class ServicePlayerListener implements Listener {
     @EventHandler
     public void onPlayerDisconnected(PlayerDisconnectEvent event) {
         ProxiedPlayer player = event.getPlayer();
+        onlinePlayer.remove(player.getUniqueId());
         for (ServiceGroup group : ServiceManager.getGroups()) {
             group.broadcast("HamsterService", "playerDisconnected %s %s", player.getUniqueId(), player.getName().toLowerCase());
         }
     }
 
-
+    @EventHandler
+    public void onServiceClientConnected(ServiceClientConnectedEvent event) {
+        for (Map.Entry<UUID, String> entry : onlinePlayer.entrySet()) {
+            event.getConnection().sendMessage("HamsterService", String.format("playerConnected %s %s", entry.getKey(), entry.getValue().toLowerCase()));
+        }
+    }
 }
